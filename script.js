d148c03d7ccc77cd3efa2c4a250cdc3e83cf1ac8
Lafoulks <foulks.lauren@gmail.com>
2021-12-15 19:54:57 -0500

    added operate function

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ function divide(a, b) {
     return (a / b);
 };
 
+function operate(operator, a, b,) {
+    if (operator === add) {
+        return (a + b);
+    } else if (operator === subtract) {
+        return (a -b);
+    } else if (operator === multiply) {
+        return (a * b);
+    } else if (operator === divide) {
+        return (a / b);
+    }
+};
