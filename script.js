b3a4c0a426a98695676681ca7c07214be0232259
Lafoulks <foulks.lauren@gmail.com>
2021-12-19 10:34:45 -0500

    added ac functionality

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const display = document.getElementById('#display');
+const topRow = document.getElementById('#top-row')
 const btns = document.querySelectorAll('.digit');
 let value = document.querySelector('p');
 const sub = document.getElementById('sub');
 const plus = document.getElementById('plus');
 const div = document.getElementById('div');
 const times = document.getElementById('times');
+const clear = document.getElementById('clear');
+const ac = document.getElementById('ac');
 
 //Operations
 function add(a, b) {
@@ -56,6 +59,14 @@ times.addEventListener('click', function () {
 
 div.addEventListener('click', function () {
     divide(a, b);
+});
+
+ac.addEventListener('click', function () {
+    value.textContent = '';
+});
+
+clear.addEventListener('click', function () {
+    //loop over btns and delete most recent one?
 });
 
 //Store first # that is input when user chooses an operator, save which operation, then operate() on them once "=" is pressed
