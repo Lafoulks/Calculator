bf890cb5833e8d69480433d3cd8d3d76889717fe
Lafoulks <foulks.lauren@gmail.com>
2021-12-20 23:03:17 -0500

    beginning functionality to addition button: displays 2 inputted values

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-const display = document.getElementById('#display');
-const topRow = document.getElementById('#top-row')
+const display = document.getElementById('display');
+const topRow = document.getElementById('top-row')
 const btns = document.querySelectorAll('.digit');
-let value = document.querySelector('p');
+let value1 = document.querySelector('p.value1');
+let value2 = document.querySelector('p.value2');
 const sub = document.getElementById('sub');
 const plus = document.getElementById('plus');
 const div = document.getElementById('div');
 const times = document.getElementById('times');
 const clear = document.getElementById('clear');
 const ac = document.getElementById('ac');
+const equals = document.getElementById('equals');
+
 
 //Operations
 function add(a, b) {
@@ -25,7 +28,7 @@ function divide(a, b) {
     return (a / b);
 };
 
-function operate(operator, a, b,) {
+function operate(operator, a, b) {
     if (operator === add) {
         return (a + b);
     } else if (operator === subtract) {
@@ -40,13 +43,24 @@ function operate(operator, a, b,) {
 //Displays numbers
 btns.forEach((btns) => {
     btns.addEventListener('click', function ()  {
-        value.textContent += btns.name;
+        value1.textContent += btns.name;
   });
 });
 
-//Stores operations to operator buttons
+
+//Must store initial number before operation is clicked. How to make a new number instead of adding on to the first number?
+//Save which operation, then have that operation be used in operate() when "=" is pressed
 plus.addEventListener('click', function () {
-    add(a, b);
+    console.log(value1); 
+    console.log(value2);
+    value1.style.display = "none";
+    //number buttons were not displaying again until I ran this code again
+    //value2 is registering inputted numbers, but they are also added on to value1 still
+    btns.forEach((btns) => {
+        btns.addEventListener('click', function ()  {
+            value2.textContent += btns.name;
+      });
+    });
 });
 
 sub.addEventListener('click', function () {
@@ -62,12 +76,15 @@ div.addEventListener('click', function () {
 });
 
 ac.addEventListener('click', function () {
-    value.textContent = '';
+    display.textContent = '';
 });
 
 clear.addEventListener('click', function () {
     //loop over btns and delete most recent one?
 });
 
-//Store first # that is input when user chooses an operator, save which operation, then operate() on them once "=" is pressed
-//After operate is called, update the display with the solution
+equals.addEventListener('click', function () {
+    console.log(value1);
+    console.log(value2);
+    //maybe store operators into object to be called
+});
