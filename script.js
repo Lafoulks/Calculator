ef2fe261d209bf1ffe6467814fe95073efaa6d6c
Lafoulks <foulks.lauren@gmail.com>
2022-04-13 21:13:18 -0400

    added AC function

--- script.js
+++ script.js
@@ -1,41 +1,52 @@
 const plus = document.getElementById('plus');
 const digit = document.querySelectorAll('.digit');
 let value = document.querySelector('p.value');
+let value2 = document.querySelector('p.value2');
 const equals = document.getElementById('equals');
+const ac = document.getElementById('ac');
+let op;
 
-//ask if this is the right way to store operators to be called upon in Operate function
-const operators = {
-    addition: function add(a, b) {
-        return a + b
-    },
-    subtraction: function subtract(a, b) {
-        return (a - b);
-    },
-    multiplication: function multiply(a, b) {
-        return (a * b);
-    },
-    division: function divide(a, b) {
-        return (a / b);
-    },
-};
 
-//this shows visual of number pressed 
+
+//shows number display when button is pressed
 digit.forEach((digit) => {
     digit.addEventListener('click', function () {
         value.textContent += digit.name;
-        console.log(value);
     });
 });
 
-//add value + a second value that will be tied to equal key? 
-//so no add function tied to this plus button
-plus.addEventListener('click', function add(a, b) {
-    value.textContent = '';
-    console.log(value);
-});
-
-
-//pull correct operator, use if/else to run though operator list? how to have it remember which operator was chosen?
-function operate() {
-
+//Math operations
+const addition = function add(a, b) {
+        return a + b
 };
+
+const subtraction = function subtract(a, b) {
+        return (a - b);
+};
+
+const multiplication = function multiply(a, b) {
+        return (a * b);
+};
+
+const division = function divide(a, b) {
+        return (a / b);
+};
+
+function operate(op, a, b) {
+    if (op = addition) {
+        return add(a,b);
+    }
+};
+
+
+//must store first number when + is pressed 
+//how to stop first numbers from adding onto value after + is pressed
+
+equals.addEventListener('click', function operate(op, a, b) {
+    console.log(value);
+    console.log(value2);
+})
+
+ac.addEventListener('click', function() {
+    value.innerHTML = '';
+});
