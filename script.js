b97605e898e785d9cae26db0f6bcf9039a280ea7
Lafoulks <foulks.lauren@gmail.com>
2022-04-17 10:18:05 -0400

    reorganized code to follow guided instruction

--- script.js
+++ script.js
@@ -1,36 +1,19 @@
-const plus = document.getElementById('plus');
 const digit = document.querySelectorAll('.digit');
-let value = document.querySelector('p.value');
-let value2 = document.querySelector('p.value2');
+const plus = document.getElementById('plus');
+const times = document.getElementById('times');
 const equals = document.getElementById('equals');
 const ac = document.getElementById('ac');
-let op;
+let display = document.querySelector('p.value');
+let op = null;
 
+//1. Create functions for all the basic math operators
 
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
 
-//shows number display when button is pressed
-digit.forEach((digit) => {
-    digit.addEventListener('click', function () {
-        value.textContent += digit.name;
-    });
-});
-
-//Math operations
-const addition = function add(a, b) {
-        return a + b
-};
-
-const subtraction = function subtract(a, b) {
-        return (a - b);
-};
-
-const multiplication = function multiply(a, b) {
-        return (a * b);
-};
-
-const division = function divide(a, b) {
-        return (a / b);
-};
+//2. Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
 
 function operate(op, a, b) {
     if (op = addition) {
@@ -38,15 +21,22 @@ function operate(op, a, b) {
     }
 };
 
+//3. Create a basic HTML calculator with buttons for each digit, each of the above functions and an “Equals” key. Add a "clear" button
 
-//must store first number when + is pressed 
-//how to stop first numbers from adding onto value after + is pressed
-
-equals.addEventListener('click', function operate(op, a, b) {
-    console.log(value);
-    console.log(value2);
-})
+digit.forEach((digit) => {
+    digit.addEventListener('click', function () {
+        display.innerHTML += digit.name;
+    });
+});
 
 ac.addEventListener('click', function() {
-    value.innerHTML = '';
+    display.innerHTML = '';
 });
+
+//4. Create the functions that populate the display when you click the number buttons
+//Store the ‘display value’ in a variable somewhere for use in the next step.
+
+//5. Store the first number that is input into the calculator when a user presses an operator, 
+//Save which operation has been chosen and then operate() on them when the user presses the “=” key.
+
+//All clear
