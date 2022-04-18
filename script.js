a62916512f7160f48210913442dad182220d2262
Lafoulks <foulks.lauren@gmail.com>
2022-04-17 21:00:32 -0400

    stored number when multiplication button is pressed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const digit = document.querySelectorAll('.digit');
 const add = document.getElementById('add');
-const subract = document.getElementById('subtract');
+const subtract = document.getElementById('subtract');
 const multiply = document.getElementById('multiply');
 const divide = document.getElementById('divide');
 const equals = document.getElementById('equals');
@@ -37,16 +37,28 @@ ac.addEventListener('click', function() {
     display.innerHTML = '';
 });
 
-//4. Create the functions that populate the display when you click the number buttons
-//Store the ‘display value’ in a variable somewhere for use in the next step.
+//4. Create the functions that populate the display. Store the ‘display value’ in a variable somewhere for use in the next step.
 
 digit.forEach((digit) => {
     digit.addEventListener('click', function () {
         display.innerHTML += digit.name;
+        num1 = display;
+        //By changing num1 to display, I store the value for later use while keeping the display visible?
+        console.log(num1);
     });
 });
 
 //5. Store the first number that is input into the calculator when a user presses an operator, 
 //Save which operation has been chosen and then operate() on them when the user presses the “=” key.
+    //Once operate has been called, update display with the solution
 
+    multiply.addEventListener('click', function() {
+        console.log(num1);
+        //Yes! Num1 is called correctly!
+    });
 
+    equals.addEventListener('click', function () {
+
+    });
+
+    //
